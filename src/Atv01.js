bb250e84c8a2cd36f784d361dd81b5df7b882421
JoaoPirasol <joaopirasol@hotmail.com>
2025-05-20 19:45:15 -0300

    Update Atv01.js

--- src/Atv01.js
+++ src/Atv01.js
@@ -1,5 +1,5 @@
-import Letreiro from "./Letreiro";
-import Relogio from "./Relogio";
+import Letreiro from "Semana1/Letreiro";
+import Relogio from "Semana1/Relogio";
 
 export default function Atv01() {
   return (
