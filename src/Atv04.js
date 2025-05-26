e70d633b1fdddd9d90b45cf30aa8973ef5df7f2f
JoaoPirasol <joaopirasol@hotmail.com>
2025-05-26 10:47:45 -0300

    Update Atv04.js

--- src/Atv04.js
+++ src/Atv04.js
@@ -4,7 +4,7 @@ import CounterList from "./CounterList";
 import Form from "./Form";
 import Form2 from "./Form2";
 import Form3 from "./Form3";
-import Gallery from "./Gallery1";
+// import Gallery from "./Gallery1";
 import List from "./List";
 import List2 from "./List2";
 import List3 from "./List3";
@@ -14,24 +14,24 @@ import ShapeEditor from "./ShapeEditor";
 import Toolbar from "./Toolbar";
 
 
-export default function Atv04() {
+export default function Atv0() {
   return (
     <>
         <h2>Semana 4</h2>
-        {/* <BucketList/>
+        <BucketList/>
         <Counter/>
         <CounterList/>
         <Form/>
         <Form2/>
         <Form3/>
-        <Gallery/>
+        {/* <Gallery/> */}
         <List/>
         <List2/>
         <List3/>
         <List4/>
         <MovingDot/>
         <ShapeEditor/>
-        <Toolbar/> */}
+        <Toolbar/>
 
     </>
   );
